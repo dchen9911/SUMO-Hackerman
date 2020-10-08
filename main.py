6edc79550af4066bc621df0b3119b79a2cc9b46b
Dennis Chen <dche9992@uni.sydney.edu.au>
2020-10-09 02:26:51 +1100

    .

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ def update_image(img):
 
 if __name__ == '__main__':
     cv2.namedWindow('Capturing', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Capturing', 800, 570)
+    # cv2.resizeWindow('Capturing', 800, 570)
+     cv2.resizeWindow('Capturing', 500, 370)
     
     ti.setup_GPIO()
 
