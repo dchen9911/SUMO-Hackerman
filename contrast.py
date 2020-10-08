c775157a2afa5b6807265fe726bf09055b058cb4
Dennis Chen <dche9992@uni.sydney.edu.au>
2020-10-09 00:45:38 +1100

    .

--- contrast.py
+++ contrast.py
@@ -26,7 +26,7 @@ def changeContrastBrightness(img, level_contrast, level_brightness):
     img = enhancer.enhance(factor_brightness)
 
     # 
-    return img
+    return numpy.array(img)
 
 def display_image(img):
     # cv2.imshow('image', self.img)
